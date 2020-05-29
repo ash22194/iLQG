af5579894d6d03d4123ac685386c82f939a3ba3a
Ashwin Rajendra Khadke <akhadke@andrew.cmu.edu>
2020-05-29 13:38:34 -0400

    Added LQR initialization

--- iLQG_boxQP/runiLQGBiped2D.m
+++ iLQG_boxQP/runiLQGBiped2D.m
@@ -16,7 +16,7 @@ sys.dt = 0.001;
 NUM_CTRL = round(sys.T / sys.dt);
 sys.gamma_ = 0.997;
 sys.Q = diag([100, 200, 2, 2, 1000, 10]);
-sys.R = 0.00001*eye(4);
+sys.R = 0.0001*eye(4);
 
 lg = 0.96;
 alpha1g = pi/2 + asin(sys.df/2/lg);
@@ -32,6 +32,42 @@ sys.u0 = [sys.m*sys.g*cos(alpha2g)/sin(alpha1g - alpha2g);
           -sys.m*sys.g*cos(alpha1g)/sin(alpha1g - alpha2g);
           0;
           0];
+
+%% Linearized system
+x = sym('x', [6, 1]); % l, alpha1, x_dot, z_dot, th, th_dot
+u = sym('u', [4, 1]); % F_leg1, F_leg2, T_hip1, T_hip2
+x_hip     = x(1)*cos(x(2));
+z_hip     = x(1)*sin(x(2));
+l2        = sqrt((x_hip + sys.df)^2 + z_hip^2);
+a2        = acos((sys.df + x_hip)/l2);
+x_hip_dot = x(3) + sys.d*cos(x(5))*x(6);
+z_hip_dot = x(4) + sys.d*sin(x(5))*x(6);
+
+zero_dyn = [0; 0; 0; -sys.g; 0; 0];
+state_dyn = [x_hip_dot*cos(x(2)) + z_hip_dot*sin(x(2));
+             (-x_hip_dot*sin(x(2)) + z_hip_dot*cos(x(2)))/x(1);
+             0;
+             0;
+             x(6);
+             0];
+act_dyn = [0;
+           0;
+           (u(1)*cos(x(2)) + u(2)*cos(a2) ...
+            + u(3)/x(1)*sin(x(2)) + u(4)/l2*sin(a2))/sys.m;
+           (u(1)*sin(x(2)) + u(2)*sin(a2) ...
+            - u(3)/x(1)*cos(x(2)) - u(4)/l2*cos(a2))/sys.m;
+           0;
+           (u(3)*(1 + sys.d/x(1)*sin(x(2) - x(6))) + u(4)*(1 + sys.d/l2*sin(a2 - x(6))) ...
+            + u(1)*sys.d*cos(x(2) - x(6)) + u(2)*sys.d*cos(a2 - x(6)))/sys.I];
+fxu = zero_dyn + state_dyn + act_dyn;
+state_dyn_x = jacobian(fxu,x);
+act_dyn_u = jacobian(fxu,u);
+A = eval(subs(state_dyn_x, [x; u], [sys.l_point; sys.u0])); 
+B = eval(subs(act_dyn_u, [x; u], [sys.l_point; sys.u0]));
+lambda_ = (1 - sys.gamma_)/sys.dt;
+[K_joint, S_joint, e_joint] = lqr(A - lambda_/2*eye(size(A,1)), B, ...
+                                  sys.Q, sys.R, ...
+                                  zeros(size(A,1), size(B,2)));
 
 % Optimization parameters
 Op.lims  = sys.lims;
@@ -85,20 +121,27 @@ timeJoint = zeros(size(x_starts, 2), 1);
 for jj=1:1:size(x_starts, 2)
     dyn_joint = @(x, u, i) biped2d_dyn_first_cst(sys_joint, x, u, sys_joint.full_DDP);
     
-    l2init = sqrt((sys.df + x_starts(1)*cos(x_starts(2)))^2 + (x_starts(1)*sin(x_starts(2)))^2);
-    alpha2init = acos((sys.df + x_starts(1)*cos(x_starts(2)))/l2init);
-    uinit = [sys.m*sys.g*cos(alpha2init)/sin(x_starts(2) - alpha2init);
-             -sys.m*sys.g*cos(x_starts(2))/sin(x_starts(2) - alpha2init);
-              0;
-              0];
-    Uinit = repmat(uinit, [1, NUM_CTRL]);
-
+%     l2init = sqrt((sys.df + x_starts(1)*cos(x_starts(2)))^2 + (x_starts(1)*sin(x_starts(2)))^2);
+%     alpha2init = acos((sys.df + x_starts(1)*cos(x_starts(2)))/l2init);
+%     uinit = [sys.m*sys.g*cos(alpha2init)/sin(x_starts(2) - alpha2init);
+%              -sys.m*sys.g*cos(x_starts(2))/sin(x_starts(2) - alpha2init);
+%               0;
+%               0];
+%     Uinit = repmat(uinit, [1, NUM_CTRL]);
+    [~, Xinit] = ode45(@(t,x) f_Biped2DFirst(sys_joint, x, sys.u0 - K_joint*(x - sys.goal)), ...
+                       linspace(0, sys.T, NUM_CTRL+1), x_starts(:,jj));
+    Xinit = Xinit';
+    Uinit = sys.u0 - K_joint*(Xinit - sys.goal);
+    discount = sys.gamma_.^linspace(0, NUM_CTRL, NUM_CTRL+1);
+    Costinit = discount.*(diag(Xinit'*sys.Q*Xinit) + diag(Uinit'*sys.R*Uinit));
+    Op.cost = Costinit;
+    
     tic;
     [XJoint(sys_joint.X_DIMS_FREE,:, jj), ...
      UJoint(:,1:NUM_CTRL, jj), ...
      KJoint(:,sys_joint.X_DIMS_FREE,1:NUM_CTRL, jj), TraceJoint{jj,1}] = iLQG(dyn_joint, ...
-                                                    x_starts(sys_joint.X_DIMS_FREE, jj), ...
-                                                    Uinit, Op);
+                                                    Xinit, ... % x_starts(sys_joint.X_DIMS_FREE, jj), ...
+                                                    Uinit(:,1:NUM_CTRL), Op);
     timeJoint(jj) = toc;
     XJoint(sys_joint.X_DIMS_FIXED,:, jj) = repmat(sys_joint.l_point(sys_joint.X_DIMS_FIXED), [1, size(XJoint,2)]);
     jj
