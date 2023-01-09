ee86956185babe596bb76a5b9ed0c93904025661
arpit agarwal <arpit15945@gmail.com>
2023-01-09 13:21:20 -0500

    Fixed the biped dynamics

--- iLQG_boxQP/new_systems/biped2d/dynu.m
+++ iLQG_boxQP/new_systems/biped2d/dynu.m
@@ -20,6 +20,8 @@ t3 = sin(x2);
 l2 = sqrt((x1.*t2 + df).^2 + (x1.*t3).^2);
 contact1 = x1 <= l0;
 contact2 = l2 <= l0;
+% contact1 = 1;
+% contact2 = 1;
 
 t4 = df.^2;
 t5 = x1.^2;
@@ -36,36 +38,29 @@ t15 = t6-1.0;
 t16 = t4+t5+t12;
 t17 = 1.0./t16;
 t18 = 1.0./sqrt(t16);
-t19 = t13.*t18;
+t19c = t13.*t18;
+t19s = x1.*t3.*t18;
 t21 = t5.*t15.*t17;
-t20 = acos(t19);
 t23 = -t21;
-t22 = -t20;
 t25 = sqrt(t23);
-t24 = t22+x5;
-
-t2 = t2.*contact1;
-t3 = t3.*contact1;
-
-t17 = t17.*contact2;
-t19 = t19.*contact2;
-t25 = t25.*contact2;
+t24c = cos(x5).*t19c + sin(x5).*t19s;
+t24s = sin(x5).*t19c - t19s.*cos(x5);
 
 out = zeros(size(x, 1), size(u, 1), size(u, 2));
-out(3,1,:) = t2.*t9;
-out(4,1,:) = t3.*t9;
+out(3,1,:) = t2.*t9.*contact1;
+out(4,1,:) = t3.*t9.*contact1;
 out(6,1,:) = d.*t7.*cos(t14).*contact1;
 
-out(3,2,:) = t9.*t19;
-out(4,2,:) = t9.*t25;
-out(6,2,:) = d.*t7.*cos(t24).*contact2;
+out(3,2,:) = t9.*t19c.*contact2;
+out(4,2,:) = t9.*t25.*contact2;
+out(6,2,:) = d.*t7.*t24c.*contact2;
 
-out(3,3,:) = t3.*t9.*t11;
-out(4,3,:) = -t2.*t9.*t11;
+out(3,3,:) = t3.*t9.*t11.*contact1;
+out(4,3,:) = -t2.*t9.*t11.*contact1;
 out(6,3,:) = t7.*t11.*(x1+d.*sin(t14)).*contact1;
 
-out(3,4,:) = t9.*t18.*t25;
-out(4,4,:) = -t9.*t13.*t17;
-out(6,4,:) = -t7.*t18.*(d.*sin(t24)-1.0./t18).*contact2;
+out(3,4,:) = t9.*t18.*t25.*contact2;
+out(4,4,:) = -t9.*t13.*t17.*contact2;
+out(6,4,:) = -t7.*t18.*(d.*t24s-1.0./t18).*contact2;
 
 end
